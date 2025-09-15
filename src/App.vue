95e7f28977ce1461d41bac7e51e2f275147d26aa
Lamis McDowall-Rose <lamism@gmail.com>
2025-09-15 21:29:55 +0100

    canvas resizing properly now

--- src/App.vue
+++ src/App.vue
@@ -2,32 +2,13 @@
 import Image from './components/Image.vue';
 import { ref, onMounted, useTemplateRef } from 'vue'
 
-// 1. mount everything 
-// 2. Go through the mounted marker and use template ref to style them /
-// 3. Profit  ?
-
 const imageName = ref("./alien_bloc_logo.jpg");
-
-function markerCanvas(markerRef) {
-    const templateCanvas = useTemplateRef(markerRef);
-    var ctx = templateCanvas.value.getContext("2d");
-    ctx.beginPath();
-    ctx.rect(0, 0, 150, 100);
-    ctx.stroke();
-}
-
-onMounted(() => {
-    markerCanvas("marker-1") // do this for all markers ?
-})
-
 const markers = ref([{
     id: 1, 
     x: 50, 
     y: 50,  
     title:""
 }]);
-
-
 </script>
 
 <template>
@@ -37,18 +18,3 @@ const markers = ref([{
     >
     </Image> 
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
--- src/components/Image.vue
+++ src/components/Image.vue
@@ -1,5 +1,6 @@
 <script setup>
-    import Marker from './Marker.vue';
+    import { ref, onMounted, useTemplateRef, watch, render, onUpdated } from 'vue'
+    import { vResizeObserver } from '@vueuse/components'
     const props = defineProps({
         imageName: {
             type: String,
@@ -7,32 +8,79 @@
         },
         markers: {
             type: Array,
-            default: []
+            default: [],
+            required: true
         },
         addMarkersOnClick: {
             type: Boolean,
             default: false
         },
     });
+
+    const imageWidth = ref(0);
+    const imageHeight = ref(0);    
+  
+    const templateCanvas = useTemplateRef("canvas");
+
+     function drawMarkers(){
+        var ctx = templateCanvas.value.getContext("2d");
+        // now, just repeat this for all the markers 
+        ctx.strokeStyle = 'black'
+        const rectangle = new Path2D();
+        rectangle.rect(imageWidth.value*(30/100) -25, imageHeight.value*(30/100)-25, 50, 50);
+        ctx.stroke(rectangle);
+    }
+
+    // I want to resize based on the actual image, not the window size, so it seems better to use this rather than a Window: resize event event
+    function onResizeObserver(entries){
+        const [entry] = entries
+        const {width, height} = entry.contentRect;
+        imageWidth.value = width;
+        imageHeight.value = height;
+    };
+
+    onMounted(() => {
+        drawMarkers();
+    });
+
+    onUpdated(() => {
+        drawMarkers();
+    });
+
 </script>
 <template>
-    <div class="imageContainer">
-        <img 
-            class="image"
-            :src="props.imageName"
-        />
-        <Marker
-            v-for="marker in props.markers"
-            :key="marker.id"
-            :markerId = marker.id
-            :markerX = marker.x
-            :markerY = marker.y 
-            :title="marker.title"
-        />
+    <div class="ImageMarkerContainer">
+        <div class="imageContainer">
+            <img v-resize-observer="onResizeObserver"
+                id="imageID"
+                class="image"
+                ref="image"
+                :src="props.imageName"
+            />
+        </div>
+        <canvas 
+            ref="canvas"
+            class="canvasClass"
+            :width=imageWidth
+            :height=imageHeight
+        ></canvas>
     </div>
 </template>
 <style>
+.ImageMarkerContainer{
+    position: relative;
+}
+
 .imageContainer{
-    position: relative
+    position: absolute;
+}
+
+.image{
+    width: 80vw;
+}
+
+.canvasClass {
+    background: v-bind(imageUrl);
+    position: absolute;
 }
 </style>
