e4b01c8886d7c72935dd0b43f0ef1507a48543f6
Lamis McDowall-Rose <lamism@gmail.com>
2025-09-19 18:01:07 +0100

    Can put multiple points on the image

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,38 @@
 import Image from './components/Image.vue';
 import { ref, onMounted, useTemplateRef } from 'vue'
 
-const imageName = ref("./alien_bloc_logo.jpg");
-const markers = ref([{
-    id: 1, 
-    x: 50, 
-    y: 50,  
-    title:""
-}]);
+const imageName = ref("./test_image.jpg");
+const canvasReference = "canvasReferenceName"
+const markers = ref([
+    {
+        markerLocationX: 50, 
+        markerLocationY: 50,  
+        title: "marker_title", 
+        canvasStyle: undefined,//{shape:"circle", size:[20], color:"black"},
+        canvasCode: undefined
+    },
+    {
+        markerLocationX: 70, 
+        markerLocationY: 70,  
+        title: "marker_title_1", 
+        canvasStyle: undefined,//{shape:"circle", size:[20], color:"black"},
+        canvasCode: "he"
+    }
+]);
+
+function canvasCodeFunction(){
+    // const templateCanvas = useTemplateRef(canvasReference);
+    console.log("Inside canvasCodeFunction function");
+    return;
+};
 </script>
 
 <template>
     <Image 
         :imageName="imageName"
         :markers="markers"
+        :canvasReference="canvasReference"
+        @canvasCodeFunction="canvasCodeFunction()"
     >
     </Image> 
 </template>
